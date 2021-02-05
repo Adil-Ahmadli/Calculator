91ace7e5d3b162340a97c686edf18d5c7a0bb127
Adil Ahmadli <74059894+aprogrammer3@users.noreply.github.com>
2021-02-05 23:13:44 +0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -45,93 +45,73 @@ if __name__ == "__main__":
 	gui = Tk() 
 
 	# set the background colour of GUI window 
-	gui.configure(background="light green") 
+	#gui.configure() 
 
 	# set the title of GUI window 
 	gui.title("Simple Calculator") 
 
-	# set the configuration of GUI window 
-	gui.geometry("270x150") 
 
 	# StringVar() is the variable class 
 	# we create an instance of this class 
 	equation = StringVar() 
 
 	# create the text entry box for showing the expression . 
-	expression_field = Entry(gui, textvariable=equation) 
+	expression_field = Entry( gui, font=('arial',20, 'bold') , textvariable=equation, bd=20, insertwidth=4, bg="powder blue", justify="right" ) 
 
 	# grid method is used for placing the widgets at respective positions in table like structure . 
-	expression_field.grid(columnspan=4, ipadx=70)
+	expression_field.grid(columnspan=4)
 
 	# create a Buttons and place at a particular  location inside the root window . 
 	# when user press the button, the command or  function affiliated to that button is executed . 
-	button1 = Button(gui, text=' 1 ', fg='black', bg='red', 
-					command=lambda: press(1), height=1, width=7) 
-	button1.grid(row=2, column=0) 
+	button1 = Button(gui, text="1", padx=22, pady=16, bd=8,  fg='black', font=("arial", 20, "bold"), bg="powder blue", command=lambda: press(1)) 
+	button1.grid(row=1, column=0)
 
-	button2 = Button(gui, text=' 2 ', fg='black', bg='red', 
-					command=lambda: press(2), height=1, width=7) 
-	button2.grid(row=2, column=1) 
+	button2 = Button(gui, text="2", padx=22,pady=16, bd=8,  fg='black', bg="powder blue", font=("arial", 20, "bold"),command=lambda: press(2)) 
+	button2.grid(row=1, column=1)
 
-	button3 = Button(gui, text=' 3 ', fg='black', bg='red', 
-					command=lambda: press(3), height=1, width=7) 
-	button3.grid(row=2, column=2) 
+	button3 = Button(gui, text="3", padx=22,pady=16, bd=8,  fg='black', bg="powder blue", font=("arial", 20, "bold"),command=lambda: press(3)) 
+	button3.grid(row=1, column=2)
 
-	button4 = Button(gui, text=' 4 ', fg='black', bg='red', 
-					command=lambda: press(4), height=1, width=7) 
-	button4.grid(row=3, column=0) 
+	button4 = Button(gui, text="4", padx=22,pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"),command=lambda: press(4)) 
+	button4.grid(row=2, column=0) 
 
-	button5 = Button(gui, text=' 5 ', fg='black', bg='red', 
-					command=lambda: press(5), height=1, width=7) 
-	button5.grid(row=3, column=1) 
+	button5 = Button(gui, text="5", padx=22,pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"),command=lambda: press(5)) 
+	button5.grid(row=2, column=1) 
 
-	button6 = Button(gui, text=' 6 ', fg='black', bg='red', 
-					command=lambda: press(6), height=1, width=7) 
-	button6.grid(row=3, column=2) 
+	button6 = Button(gui, text="6"  ,padx=22,pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"), command=lambda: press(6) ) 
+	button6.grid(row=2, column=2) 
 
-	button7 = Button(gui, text=' 7 ', fg='black', bg='red', 
-					command=lambda: press(7), height=1, width=7) 
-	button7.grid(row=4, column=0) 
+	button7 = Button(gui, text="7", padx=22,pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"), command=lambda: press(7)) 
+	button7.grid(row=3, column=0) 
 
-	button8 = Button(gui, text=' 8 ', fg='black', bg='red', 
-					command=lambda: press(8), height=1, width=7) 
-	button8.grid(row=4, column=1) 
+	button8 = Button(gui, text="8" ,padx=22,pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"),command=lambda: press(8)) 
+	button8.grid(row=3, column=1) 
 
-	button9 = Button(gui, text=' 9 ', fg='black', bg='red', 
-					command=lambda: press(9), height=1, width=7) 
-	button9.grid(row=4, column=2) 
+	button9 = Button(gui, text="9", padx=22,pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"),command=lambda: press(9)) 
+	button9.grid(row=3, column=2) 
 
-	button0 = Button(gui, text=' 0 ', fg='black', bg='red', 
-					command=lambda: press(0), height=1, width=7) 
-	button0.grid(row=5, column=0) 
+	button0 = Button(gui, text="0", padx=22,pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"),command=lambda: press(0)) 
+	button0.grid(row=4, column=0) 
 
-	plus = Button(gui, text=' + ', fg='black', bg='red', 
-				command=lambda: press("+"), height=1, width=7) 
-	plus.grid(row=2, column=3) 
+	plus = Button(gui, text='+', padx=22, pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"),	command=lambda: press("+")) 
+	plus.grid(row=1, column=3) 
 
-	minus = Button(gui, text=' - ', fg='black', bg='red', 
-				command=lambda: press("-"), height=1, width=7) 
-	minus.grid(row=3, column=3) 
+	minus = Button(gui, text='-', padx=22,pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"),command=lambda: press("-")) 
+	minus.grid(row=2, column=3) 
 
-	multiply = Button(gui, text=' * ', fg='black', bg='red', 
-					command=lambda: press("*"), height=1, width=7) 
-	multiply.grid(row=4, column=3) 
+	multiply = Button(gui, text='*', padx=22,pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"),command=lambda: press("*")) 
+	multiply.grid(row=3, column=3) 
 
-	divide = Button(gui, text=' / ', fg='black', bg='red', 
-					command=lambda: press("/"), height=1, width=7) 
-	divide.grid(row=5, column=3) 
+	divide = Button(gui, text='/', padx=22,pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"),command=lambda: press("/")) 
+	divide.grid(row=4, column=3) 
 
-	equal = Button(gui, text=' = ', fg='black', bg='red', 
-				command=equalpress, height=1, width=7) 
-	equal.grid(row=5, column=2) 
+	equal = Button(gui, text='=', padx=22,pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"),command=equalpress) 
+	equal.grid(row=4, column=2) 
 
-	clear = Button(gui, text='Clear', fg='black', bg='red', 
-				command=clear, height=1, width=7) 
-	clear.grid(row=5, column='1') 
+	clear = Button(gui, text='C', padx=22,pady=16, bd=8, fg='black',bg="powder blue", font=("arial", 20, "bold"),	command=clear) 
+	clear.grid(row=4, column=1) 
 
-	Decimal= Button(gui, text='.', fg='black', bg='red', 
-					command=lambda: press('.'), height=1, width=7) 
-	Decimal.grid(row=6, column=0) 
+	#Decimal= Button(gui, text='.', padx=16, bd=8, fg='black', font=("arial", 20, "bold"),command=lambda: press('.')) 
+	#Decimal.grid(row=6, column=0) 
 	# start the GUI 
-	gui.mainloop() 
-
+	gui.mainloop()
