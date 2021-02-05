e831d50bf147cea36f149beed1c165504074217a
Adil Ahmadli <74059894+aprogrammer3@users.noreply.github.com>
2021-02-05 11:55:56 +0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,12 +9,8 @@ expression = ""
 
 # Function to update expression in the text entry box 
 def press(num): 
-	# point out the global expression variable 
 	global expression 
-
-	# concatenation of string 
 	expression = expression + str(num) 
-
 	# update the expression by using set method 
 	equation.set(expression) 
 
@@ -22,17 +18,11 @@ def press(num):
 # Function to evaluate the final expression 
 def equalpress(): 
 	# Try and except statement is used  for handling the errors like zero division error etc. 
-	# Put that code inside the try block which may generate the error 
 	try: 
 
-		global expression 
-
-		# eval function evaluates the expression and str function converts the result into string 
+		global expression  
 		total = str(eval(expression)) 
-
-		equation.set(total) 
-
-		# initialze the expression variable  by empty string 
+		equation.set(total)  
 		expression = "" 
 
 	# if error is generate then handle by the except block 
